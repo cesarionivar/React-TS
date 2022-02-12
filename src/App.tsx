3ea5a72229e61607558cc5eba8bb186a367a7f8c
Cesario Nivar <ing.cesarionivar@gmail.com>
2022-02-12 15:53:53 -0400

    useEffect + useRef

--- src/App.tsx
+++ src/App.tsx
@@ -1,14 +1,22 @@
 import { Counter } from './components/Counter';
 import { Usuario } from './components/Usuario';
 
+import { TimerPadre } from './components/TimerPadre';
+
 function App() {
   return (
     <>
       <h1>React + Typescript</h1>
       <hr />
 
+      <h2>useState</h2>
       <Counter />
       <Usuario />
+
+      <h2 className='mt-5'>UseEffect</h2>
+      <hr />
+
+      <TimerPadre />
     </>
   );
 }
